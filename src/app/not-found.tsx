7edee50a5838a7ffb35b6fd8578597758d48baac
naufalhanif25 <naufal.hanif2525@gmail.com>
2026-07-30 09:30:47 +0700

    feat: Add not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,36 @@
+"use client"
+
+import { cn } from "./lib/cn"
+import { ArrowLeft } from "lucide-react"
+import { useRouter } from "next/navigation"
+
+export default function NotFound() {
+    const router = useRouter()
+
+    return (
+        <div
+            className={cn(
+                "w-dvw h-dvh p-8 gap-8",
+                "flex flex-col items-center justify-center",
+                "overflow-hidden"
+            )}
+        >
+            <div className={cn("flex flex-col items-center justify-center", "w-fit h-fit gap-2")}>
+                <h1 className="text-center text-8xl font-semibold">404</h1>
+                <h4 className="text-center font-medium">Halaman Tidak Ditemukan</h4>
+            </div>
+            <button
+                onClick={() => router.back()}
+                className={cn(
+                    "h-10 px-5 rounded-md text-sm",
+                    "cursor-pointer overflow-hidden",
+                    "flex items-center justify-center gap-2",
+                    "bg-indigo-400 text-white"
+                )}
+            >
+                <ArrowLeft strokeWidth={2} className="size-4 text-white shrink-0" />
+                Kembali
+            </button>
+        </div>
+    )
+}
