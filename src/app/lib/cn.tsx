c7a3cf105f8d232f90ca5146ff5113e0cfa1012a
naufalhanif25 <naufal.hanif2525@gmail.com>
2026-07-30 09:32:27 +0700

    chore: Add project lib (cn)

---
+++ src/app/lib/cn.tsx
@@ -0,0 +1,8 @@
+"use client"
+
+import { type ClassValue, clsx } from "clsx"
+import { twMerge } from "tailwind-merge"
+
+export function cn(...classNames: ClassValue[]): string {
+    return twMerge(clsx(classNames))
+}
