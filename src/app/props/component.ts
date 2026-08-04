b2ac218e4bde312b36080f6ad0121d1362cf47ac
naufalhanif25 <naufal.hanif2525@gmail.com>
2026-08-04 08:21:55 +0700

    fea: Add new types and interfaces

--- src/app/props/component.ts
+++ src/app/props/component.ts
@@ -1,29 +1,45 @@
-import { FileText, ClipboardClock, FileStack, SquarePen, Trash, ZoomIn, Save } from "lucide-react"
-import { HTMLAttributes, InputHTMLAttributes } from "react"
-import { ReactNode } from "react"
+import { FileText, ClipboardClock } from "lucide-react"
+import { HTMLAttributes, InputHTMLAttributes, TextareaHTMLAttributes } from "react"
+import * as z from "zod"
 
-export interface HomeCheckBoxProps extends HTMLAttributes<HTMLSpanElement> {
+export interface CheckBoxProps extends HTMLAttributes<HTMLSpanElement> {
     active: boolean
+    title?: string
+}
+
+export interface TextAreaProps extends TextareaHTMLAttributes<HTMLTextAreaElement> {
     title: string
 }
 
-export interface HomeInputProps extends InputHTMLAttributes<HTMLInputElement> {
+export interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
     title: string
 }
 
-export type DropdownProps = HTMLAttributes<HTMLSpanElement>
-
-export interface DropdownButtonProps extends HTMLAttributes<HTMLButtonElement> {
+export interface DropdownProps extends HTMLAttributes<HTMLDivElement> {
+    onChoose?: (value: string) => void
     title: string
-    icon: ReactNode
+    placeholder: string
+    options: string[]
+    value?: string | null
 }
 
 export const ComponentRegistry = {
     FileText,
     ClipboardClock,
-    FileStack,
-    SquarePen,
-    Trash,
-    ZoomIn,
-    Save
 }
+
+export interface TableHeadProps extends HTMLAttributes<HTMLTableSectionElement> {
+    names: string[]
+}
+
+export type Division = "Intel" | "Pembinaan" | "Pidum" | "Pidsus" | "PAPBB" | "Datun"
+
+export interface DocumentInitProps {
+    name: string
+    division: Division | null
+}
+
+export const DocumentInit = z.object({
+    name: z.string(),
+    division: z.enum(["Intel", "Pembinaan", "Pidum", "Pidsus", "PAPBB", "Datun"]).nullable(),
+})
--- src/app/props/dashboard.ts
+++ src/app/props/dashboard.ts
@@ -1,5 +1,6 @@
 import * as z from "zod"
-import { HTMLAttributes, ReactNode } from "react"
+import { ChangeEvent, HTMLAttributes, ReactNode } from "react"
+import { DocumentInitProps } from "./component"
 
 export type DashboardProps = HTMLAttributes<HTMLDivElement> & {
     children: ReactNode
@@ -33,4 +34,69 @@ export const DashboardSubPage = z.object({
 
 export interface RoundButtonProps extends HTMLAttributes<HTMLSpanElement> {
     icon: ReactNode
+}
+
+export interface PicketBoxProps extends HTMLAttributes<HTMLSpanElement> {
+    name: string
+    nip: string
+    lastRemind?: string | null
+    onDone?: () => void
+    onRemind?: () => void
+}
+
+export interface MainSPTJBProps extends HTMLAttributes<HTMLDivElement> {
+    onCreate?: () => void
+    onChoose?: (value: string) => void
+    onNameChange?: (event: ChangeEvent<HTMLInputElement, HTMLInputElement>) => void
+    data: DocumentInitProps
+}
+
+export interface NewSPTJBProps extends HTMLAttributes<HTMLDivElement> {
+    onBack?: () => void
+    onDelete?: () => void
+    onSave?: () => void
+    onPrint?: () => void
+    onChoose?: (value: string) => void
+    onNameChange?: (event: ChangeEvent<HTMLInputElement, HTMLInputElement>) => void
+    data: DocumentInitProps
+}
+
+export interface FormSectionProps extends HTMLAttributes<HTMLDivElement> {
+    title: string
+}
+
+export interface FormInputProps {
+    code: string
+    name: string
+    description: string
+    date: Date
+    total: number
+}
+
+export const FormInput = z.object({
+    code: z.string(),
+    name: z.string(),
+    description: z.string(),
+    date: z.date(),
+    total: z.number(),
+})
+
+export interface AdditionalFormProps extends HTMLAttributes<HTMLSpanElement> {
+    onChoose?: (value: string) => void
+    onNameChange?: (
+        event: ChangeEvent<
+            HTMLInputElement | HTMLTextAreaElement,
+            HTMLInputElement | HTMLTextAreaElement
+        >
+    ) => void
+    onDescChange?: (
+        event: ChangeEvent<
+            HTMLInputElement | HTMLTextAreaElement,
+            HTMLInputElement | HTMLTextAreaElement
+        >
+    ) => void
+    onDateChange?: (event: ChangeEvent<HTMLInputElement, HTMLInputElement>) => void
+    onTotalChange?: (event: ChangeEvent<HTMLInputElement, HTMLInputElement>) => void
+    title: string
+    data: FormInputProps
 }
