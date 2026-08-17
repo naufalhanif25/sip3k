e7eef41e0605aae846329a84c13cc0b56b5b5b00
naufalhanif25 <naufal.hanif2525@gmail.com>
2026-08-17 16:43:55 +0700

    feat: Add document template endpoints

---
+++ src/app/api/document/template/route.ts
@@ -0,0 +1,94 @@
+import { NextResponse } from "next/server"
+import { TEMPLATE_PATH } from "@/app/props/sptjb"
+import { type ExcelCellProps, ExcelCell } from "@/app/props/api"
+import { JSONFilePreset } from "lowdb/node"
+import { DEFAULT_DATA, DataBase } from "@/app/props/db"
+import ExcelJS from "exceljs"
+import { getDocumentCode } from "@/app/lib/sptjb-utils-handler"
+import { cookies } from "next/headers"
+import { verifyJWT } from "@/app/lib/global-utils"
+import { writeLog } from "@/app/lib/logs-utils"
+import { SERVER_ERROR_RESPONSE, UNAUTHORIZED_RESPONSE } from "@/app/vars/db-vars"
+
+export async function GET() {
+    try {
+        const cookieStore = await cookies()
+        const token = cookieStore.get("token")?.value
+        const isValidToken = token ? await verifyJWT(token) : null
+
+        if (!isValidToken) {
+            return UNAUTHORIZED_RESPONSE
+        }
+        const workbook = new ExcelJS.Workbook()
+        await workbook.xlsx.readFile(TEMPLATE_PATH)
+        const worksheet = workbook.worksheets[0]
+        if (!worksheet) throw new Error("Worksheet tidak ditemukan.")
+
+        const merges = worksheet.model.merges || []
+        let documentCode: string = ""
+        const columns =
+            worksheet.columns?.map((col) => ({
+                width: col.width || 10,
+            })) || []
+        const rowsData: ExcelCellProps[][] = []
+
+        worksheet.eachRow({ includeEmpty: true }, (row, rowNumber) => {
+            const rowCells: ExcelCellProps[] = []
+
+            row.eachCell({ includeEmpty: true }, (cell, colNumber) => {
+                const parsedValue = getDocumentCode(String(cell.result ?? cell.value ?? ""))
+                if (parsedValue.isTrue) documentCode = parsedValue.value
+                const finalValue = `${parsedValue.prefix || ""}${parsedValue.value}`
+
+                rowCells[colNumber - 1] = ExcelCell.parse({
+                    value: finalValue,
+                    type: cell.type || ExcelJS.ValueType.String,
+                    alignment: {
+                        horizontal: cell.alignment?.horizontal,
+                        vertical: cell.alignment?.vertical,
+                        wrapText: Boolean(cell.alignment?.wrapText),
+                    },
+                    font: cell.font || {},
+                    fill: cell.fill || {},
+                    border: cell.border || {},
+                })
+            })
+            rowsData[rowNumber - 1] = rowCells
+        })
+        const headerFooter = worksheet.headerFooter || {}
+        const lastRow = rowsData[rowsData.length - 1]
+        const lastCell = lastRow[lastRow.length - 1]
+        const db = await JSONFilePreset("data/db.json", DEFAULT_DATA)
+        const data = DataBase.parse(db.data)
+
+        return NextResponse.json({
+            success: true,
+            data: {
+                code: documentCode,
+                length: data.sptjb.length,
+                merges,
+                rows: rowsData,
+                columns,
+                headerFooter: {
+                    font: {
+                        name: lastCell.font.name,
+                        size: lastCell.font.size,
+                        family: lastCell.font.family,
+                    },
+                    differentFirst: headerFooter.differentFirst || false,
+                    differentOddEven: headerFooter.differentOddEven || false,
+                    oddHeader: headerFooter.oddHeader || "",
+                    oddFooter: headerFooter.oddFooter || "",
+                    evenHeader: headerFooter.evenHeader || "",
+                    evenFooter: headerFooter.evenFooter || "",
+                    firstHeader: headerFooter.firstHeader || "",
+                    firstFooter: headerFooter.firstFooter || "",
+                },
+            },
+        })
+    } catch (err) {
+        console.error(err)
+        await writeLog(err, "ERROR")
+        return SERVER_ERROR_RESPONSE
+    }
+}
