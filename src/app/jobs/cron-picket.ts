a8493ebcc8f50e3b05178ef59e28aabf750a8383
naufalhanif25 <naufal.hanif2525@gmail.com>
2026-08-20 12:02:28 +0700

    refactor: Use global variable instead of hard coded it

--- src/app/jobs/cron-picket.ts
+++ src/app/jobs/cron-picket.ts
@@ -1,19 +1,23 @@
 import { Cron } from "croner"
 import { JSONFilePreset } from "lowdb/node"
 import { DataBase } from "@/app/props/db"
-import { DEFAULT_DATA } from "@/app/vars/db-vars"
 import { generatePicketSchedule, sendDirectWAMessage } from "@/app/lib/picket-db-handler"
 import { writeLog } from "@/app/lib/logs-utils"
 import { matchDate } from "@/app/lib/global-utils"
-import { TIMEZONE } from "@/app/vars/db-vars"
 import { dateTZ } from "@/app/lib/date-timezone"
+import { DEFAULT_DATA, DATABASE_PATH } from "@/app/vars/db-vars"
+import { TIMEZONE } from "@/app/vars/db-vars"
+import path from "path"
 
 new Cron("0 8 * * *", { timezone: TIMEZONE }, async () => {
     const today = dateTZ.now()
 
     if (today.date() === today.daysInMonth()) {
         try {
-            const db = await JSONFilePreset("data/db.json", DataBase.parse(DEFAULT_DATA))
+            const db = await JSONFilePreset(
+                path.resolve(process.cwd(), DATABASE_PATH),
+                DataBase.parse(DEFAULT_DATA)
+            )
             const data = DataBase.parse(db.data)
             const { existingBatchIndex, newPicket } = generatePicketSchedule(data)
 
@@ -36,7 +40,10 @@ new Cron("0 10 * * *", { timezone: TIMEZONE }, async () => {
     const tomorrow = dateTZ.now().add(1, "day").toDate()
 
     try {
-        const db = await JSONFilePreset("data/db.json", DataBase.parse(DEFAULT_DATA))
+        const db = await JSONFilePreset(
+            path.resolve(process.cwd(), DATABASE_PATH),
+            DataBase.parse(DEFAULT_DATA)
+        )
         const data = DataBase.parse(db.data)
         if (!data.pickets || data.pickets.length === 0) return
         const targetPicket = data.pickets.find((value) =>
